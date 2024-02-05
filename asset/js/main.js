c29d234bf0c1f706465c14c31e50b36710a37fc5
Yi-Wen Chen <claire1212@gmail.com>
2024-02-05 23:42:34 +0800

    add skill & lottie

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,1 +1,7 @@
-import '../scss/all.css'
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById("lottieAnimation"), //要放的容器名稱
+    renderer: "svg", //
+    loop: true, //重複動畫
+    autoplay: true, //自動播放
+    path: "data.json" //要讀取的json檔案位置
+});
